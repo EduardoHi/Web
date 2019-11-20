330506ab5ec5eacb115ccd8ccabe8da58051ccf0
EduardoHi <eshidalgo97@gmail.com>
2019-11-19 21:53:39 -0600

    rm dead code

--- lab7_crud/src/models/person.js
+++ lab7_crud/src/models/person.js
@@ -23,40 +23,6 @@ const personSchema = Schema({
     spouse: String,
 });
 
-
-// userSchema.statics.findByCredentials = function(email, password)
-// {
-//   return new Promise( function(resolve, reject) {
-//     User.findOne({ email }).then(function(user) {
-//       if (!user) {
-//         return reject('User does not exist')
-//       }
-//       bcryptjs.compare(password, user.password).then(function (match) {
-//         if( match ) {
-//           resolve(user)
-//         }
-//         reject('Wrong user or password')
-//       }).catch( function(error) {
-//         reject('Wrong user or password')
-//       })
-//     })
-//   })
-// }
-
-// userSchema.pre('save', function(next) {
-//   const user = this
-//   if (user.isModified('password') ) {
-//     bcryptjs.hash(user.password, 8).then(function(hash) {
-//       user.password = hash
-//       next()
-//     }).catch(function(error) {
-//       return next(error)
-//     })
-//   } else {
-//     next()
-//   }
-// })
-
 const Person = mongoose.model('Person', personSchema)
 
 module.exports = Person
